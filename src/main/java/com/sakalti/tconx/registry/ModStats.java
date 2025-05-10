fee731c9577431dce07ec79ab6a8ea9ec4108581
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-10 16:08:48 +0900

    Create ModStats.java

---
+++ src/main/java/com/sakalti/tconx/registry/ModStats.java
@@ -0,0 +1,14 @@
+package com.sakalti.tconx.registry;
+
+import slimeknights.tconstruct.library.materials.stats.*;
+
+public class ModStats {
+
+    public static final HeadMaterialStats VIBERIUM_HEAD = new HeadMaterialStats(1320, 10.5f, 3.5f, 5);
+    public static final HandleMaterialStats VIBERIUM_HANDLE = new HandleMaterialStats(1.60f, 0);
+    public static final ExtraMaterialStats VIBERIUM_EXTRA = new ExtraMaterialStats(1.85f);
+
+    public static final HeadMaterialStats NEHILIUM_HEAD = new HeadMaterialStats(1136, 11.5f, 3.25f, 6);
+    public static final HandleMaterialStats NEHILIUM_HANDLE = new HandleMaterialStats(1.75f, 0);
+    public static final ExtraMaterialStats NEHILIUM_EXTRA = new ExtraMaterialStats(1.75f);
+}
