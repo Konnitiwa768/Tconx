1f14f8c4e8aa614ebbaec116521039298d1afeb5
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-10 16:10:24 +0900

    Create ModMaterials.java

---
+++ src/main/java/com/sakalti/tconx/registry/ModMaterials.java
@@ -0,0 +1,8 @@
+package com.sakalti.tconx.registry;
+
+import slimeknights.tconstruct.library.materials.definition.MaterialId;
+
+public class ModMaterials {
+    public static final MaterialId VIBERIUM = new MaterialId("tconx", "viberium");
+    public static final MaterialId NEHILIUM = new MaterialId("tconx", "nehilium");
+}
