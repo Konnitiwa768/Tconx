504ea7c4ed661bf30e96b1391b4e39c02e22a9d4
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-22 20:49:24 +0900

    Update ModStats.java

--- src/main/java/com/sakalti/tconx/registry/ModStats.java
+++ src/main/java/com/sakalti/tconx/registry/ModStats.java
@@ -11,4 +11,21 @@ public class ModStats {
     public static final HeadMaterialStats NEHILIUM_HEAD = new HeadMaterialStats(1136, 11.5f, 3.25f, 6);
     public static final HandleMaterialStats NEHILIUM_HANDLE = new HandleMaterialStats(1.75f, 0);
     public static final ExtraMaterialStats NEHILIUM_EXTRA = new ExtraMaterialStats(1.75f);
+        // 緑色Keriumのヘッドパーツ (耐久, 掘る速度, harvestレベル, 基本攻撃力)
+    HeadMaterialStats keriumGreenHead = new HeadMaterialStats(
+        742,
+        7.5f,
+        4,
+        3.45f
+    );
+
+    // ハンドルパーツ (倍率, 追加耐久)
+    HandleMaterialStats keriumGreenHandle = new HandleMaterialStats(
+        1.88f,
+    );
+
+    // エクストラパーツ (倍率)
+    ExtraMaterialStats keriumGreenExtra = new ExtraMaterialStats(
+        1.45f
+    );
 }
