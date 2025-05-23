bebf021515703694094becfbf2c1aa5092ae17af
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-23 22:02:37 +0900

    Create kerium_alloy.js

---
+++ src/main/resources/data/tconx/tconstruct/alloys/kerium_alloy.js
@@ -0,0 +1,18 @@
+{
+  "type": "tconstruct:alloy",
+  "inputs": [
+    {
+      "name": "tconx:molten_viberium",
+      "amount": 175
+    },
+    {
+      "name": "tconx:molten_iron",
+      "amount": 100
+    }
+  ],
+  "result": {
+    "fluid": "tconx:molten_kerium",
+    "amount": 50
+  },
+  "temperature": 1350
+}
