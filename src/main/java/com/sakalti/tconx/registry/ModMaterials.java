8ecc079945d2eac6be70b06eeaf8b1646aedc119
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-23 21:56:10 +0900

    Update ModMaterials.java

--- src/main/java/com/sakalti/tconx/registry/ModMaterials.java
+++ src/main/java/com/sakalti/tconx/registry/ModMaterials.java
@@ -6,4 +6,5 @@ public class ModMaterials {
     public static final MaterialId VIBERIUM = new MaterialId("tconx", "viberium");
     public static final MaterialId NEHILIUM = new MaterialId("tconx", "nehilium");
     public static final MaterialId KERIUM = new MaterialId("tconx", "kerium");
+    public static final MaterialId URI = new MaterialId("tconx", "uri");
 }
