d09055285a49a291d9db32ab9b80c83480e00ab3
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-29 08:29:07 +0900

    Update TconXMod.java

--- src/main/java/com/sakalti/tconx/TconXMod.java
+++ src/main/java/com/sakalti/tconx/TconXMod.java
@@ -1,13 +1,27 @@
 package com.sakalti.tconx;
 
-import com.sakalti.tconx.registry.ModMaterialIntegration;
-import net.fabricmc.api.ModInitializer;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-public class TconXMod implements ModInitializer {
+import com.sakalti.tconx.registry.ModMaterialIntegration;
+
+@Mod(TConX.MOD_ID)
+public class TConXMod {
     public static final String MOD_ID = "tconx";
 
-    @Override
-    public void onInitialize() {
-        ModMaterialIntegration.registerAll();
+    public TConXMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // MODのセットアップイベント登録
+        modEventBus.addListener(this::setup);
+
+        // 必要ならここでアイテムやツール登録の初期処理を呼び出し
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // Tinkers' Construct APIとの連携や、初期化処理を書く場所
+        // 例: TinkerRegistryにレシピ追加など
     }
 }
