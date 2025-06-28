f584fa543381ce960280d97163dd79f7ecee237b
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-29 08:45:54 +0900

    Update ModMaterialIntegration.java

--- src/main/java/com/sakalti/tconx/registry/ModMaterialIntegration.java
+++ src/main/java/com/sakalti/tconx/registry/ModMaterialIntegration.java
@@ -1,12 +1,12 @@
 package com.sakalti.tconx.registry;
 
 import com.sakalti.tconx.TconXMod;
-import com.sakalti.tconx.modifier.*
+import com.sakalti.tconx.modifier.*;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import slimeknights.tconstruct.library.materials.*;
 import slimeknights.tconstruct.library.materials.stats.*;
 import slimeknights.tconstruct.library.utils.TagUtil;
+import slimeknights.tconstruct.library.modifiers.ModifierRegistry;
 
 public class ModMaterialIntegration {
 
@@ -15,15 +15,14 @@ public class ModMaterialIntegration {
         registerMaterial(ModMaterials.NEHILIUM, 0x9370DB, ModStats.NEHILIUM_HEAD, ModStats.NEHILIUM_HANDLE, ModStats.NEHILIUM_EXTRA);
         registerMaterial(ModMaterials.KERIUM, 0x44ee44, ModStats.KERIUM_HEAD, ModStats.KERIUM_HANDLE, ModStats.KERIUM_EXTRA);
         registerMaterial(ModMaterials.URI, 0xFFD700, ModStats.URI_HEAD, ModStats.URI_HANDLE, ModStats.URI_EXTRA);
-        registerMaterial(ModMaterials.HACHILIGHT, 0x00eeeee, ModStats.HACHILIGHT_HEAD, ModStats.HACHILIGHT_HANDLE, ModStats.HACHILIGHT_EXTRA);
 
+        ModifierRegistry.register(new Identifier("tconx", "viberium_modifier"), new ViberiumModifier());
+        ModifierRegistry.register(new Identifier("tconx", "nehilium_modifier"), new NehiliumModifier());
+        ModifierRegistry.register(new Identifier("tconx", "kerium_modifier"), new KeriumModifier());
+        ModifierRegistry.register(new Identifier("tconx", "uri_modifier"), new UriModifier());
+        ModifierRegistry.register(new Identifier("tconx", "hachilight_modifier"), new HachilightModifier());
     }
-    ModifierRegistry.register(new Identifier("tconx", "viberium_modifier"), new ViberiumModifier());
-    ModifierRegistry.register(new Identifier("tconx", "nehilium_modifier"), new NehiliumModifier());
-    ModifierRegistry.register(new Identifier("tconx", "kerium_modifier"), new KeriumModifier());
-    ModifierRegistry.register(new Identifier("tconx", "uri_modifier"), new UriModifier());
-    ModifierRegistry.register(new Identifier("tconx", "hachilight_modifier"), new HachilightModifier());
-   
+
     private static void registerMaterial(MaterialId id, int color,
                                          HeadMaterialStats head, HandleMaterialStats handle, ExtraMaterialStats extra) {
         Material material = new Material(id, color, false); // false = not craftable in Part Builder
@@ -31,7 +30,6 @@ public class ModMaterialIntegration {
         MaterialRegistry.getInstance().addMaterial(material);
         MaterialRegistry.getInstance().addMaterialStats(id, head, handle, extra);
 
-        // 代表アイテム（仮）: 後で ingot 登録する場合はこちら変更
         material.setRepresentativeItemTag(TagUtil.createItemTag(new Identifier(id.getNamespace(), "ingots/" + id.getPath())));
     }
 }
