bec12590d81d800a18ea02154ea202a3ba53122e
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-01 08:42:46 +0900

    Update ModStats.java

--- src/main/java/com/sakalti/tconx/material/hachilite/ModStats.java
+++ src/main/java/com/sakalti/tconx/material/hachilite/ModStats.java
@@ -5,10 +5,10 @@ import slimeknights.tconstruct.library.tools.stat.ToolStats;
 public class ModStats {
 
     public static HeadMaterialStats HACHILITE_HEAD = new HeadMaterialStats(
-        800, 6.5f, 3.2f, 4); // 耐久力, 掘削速度, 攻撃力, 精度
+        950, 6.5f, 3.2f, 4); // 耐久力, 掘削速度, 攻撃力, 精度
 
     public static HandleMaterialStats HACHILITE_HANDLE = new HandleMaterialStats(
-        1.1f, 20); // 攻撃速度補正, 耐久増加
+        1.5f, 20); // 攻撃速度補正, 耐久増加
 
     public static ExtraMaterialStats HACHILITE_EXTRA = new ExtraMaterialStats(50); // 追加耐久力
 
