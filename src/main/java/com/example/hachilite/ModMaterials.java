a370afb766e11f60783c4e9bcb83bd5692ce1e9a
Konnitiwa768 <172898984+Konnitiwa768@users.noreply.github.com>
2025-06-30 22:43:41 +0000

    Auto: Generate item jsons

---
+++ src/main/java/com/example/hachilite/ModMaterials.java
@@ -0,0 +1,20 @@
+package com.example.hachilite;
+
+import net.minecraft.resources.ResourceLocation;
+import slimeknights.tconstruct.library.materials.Material;
+import slimeknights.tconstruct.library.materials.MaterialId;
+import slimeknights.tconstruct.library.materials.definition.MaterialManager;
+
+public class ModMaterials {
+    public static final MaterialId HACHILITE_ID = new MaterialId("hachilite");
+    public static Material HACHILITE;
+
+    public static void registerMaterials() {
+        HACHILITE = MaterialManager.getInstance().register(
+            new Material(HACHILITE_ID, Material.DisplayName.withTranslationKey("material.hachilite"))
+                .setCraftable(true)
+                .setCastable(true)
+                .setFluid(new ResourceLocation("tconstruct", "molten_hachilite"))
+        );
+    }
+}
