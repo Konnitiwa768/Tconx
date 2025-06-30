a7b8fb1b5e3707799f6390af78929acd1949f151
Konnitiwa768 <172898984+Konnitiwa768@users.noreply.github.com>
2025-06-30 23:00:32 +0000

    Auto: Generate item jsons

---
+++ src/main/java/com/sakalti/tconx/ModMain.java
@@ -0,0 +1,17 @@
+package com.sakalti.tconx;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import com.sakalti.tconx.material.hachilite.ModMaterials;
+import com.sakalti.tconx.material.hachilite.ModStats;
+
+@Mod("tconx")
+public class ModMain {
+
+    public ModMain() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModMaterials.registerMaterials(bus);
+        ModStats.registerStats();
+    }
+}
