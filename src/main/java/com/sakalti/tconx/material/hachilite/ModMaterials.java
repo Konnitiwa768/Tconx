5bea4d3a921de6739035e2470da4746b264f9997
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-01 16:00:59 +0900

    Update ModMaterials.java

--- src/main/java/com/sakalti/tconx/material/hachilite/ModMaterials.java
+++ src/main/java/com/sakalti/tconx/material/hachilite/ModMaterials.java
@@ -4,21 +4,19 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import slimeknights.tconstruct.library.materials.definition.MaterialDefinition;
 import slimeknights.tconstruct.library.materials.definition.MaterialRegistry;
-import slimeknights.tconstruct.library.materials.definition.MaterialVariantId;
+import slimeknights.tconstruct.library.materials.definition.MaterialId;
 
 public class ModMaterials {
 
-    public static final MaterialVariantId HACHILITE_ID = MaterialVariantId.create("tconx:hachilite");
+    public static final MaterialId HACHILITE_ID = new MaterialId("tconx:hachilite");
     public static MaterialDefinition HACHILITE;
 
     public static void registerMaterials(IEventBus bus) {
-        HACHILITE = MaterialRegistry.register(HACHILITE_ID, builder -> {
-            builder.setCraftable(true);
-            builder.setCastable(true);
-            builder.setFluid(new ResourceLocation("tconx", "molten_hachilite"));
-            builder.setTrait(new ResourceLocation("tconx", "hachilite_slow"));
-            builder.setStats(ModStats::applyToolStats);
-            builder.setArmorStats(ModStats::applyArmorStats);
+        HACHILITE = MaterialRegistry.register(HACHILITE_ID, def -> {
+            def.setCraftable(true);
+            def.setCastable(true);
+            def.setFluid(new ResourceLocation("tconx", "molten_hachilite"));
+            def.addTrait(new ResourceLocation("tconx", "hachilite_slow"));
         });
     }
 }
