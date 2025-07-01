c1daf44fe85898bf8d278bc4c61bd14a01c36d03
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-01 16:19:52 +0900

    Update ModMaterials.java

--- src/main/java/com/sakalti/tconx/material/hachilite/ModMaterials.java
+++ src/main/java/com/sakalti/tconx/material/hachilite/ModMaterials.java
@@ -1,22 +1,12 @@
 package com.sakalti.tconx.material.hachilite;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.eventbus.api.IEventBus;
-import slimeknights.tconstruct.library.materials.definition.MaterialDefinition;
-import slimeknights.tconstruct.library.materials.definition.MaterialRegistry;
-import slimeknights.tconstruct.library.materials.definition.MaterialId;
+import slimeknights.tconstruct.library.materials.MaterialId;
 
 public class ModMaterials {
 
     public static final MaterialId HACHILITE_ID = new MaterialId("tconx:hachilite");
-    public static MaterialDefinition HACHILITE;
 
-    public static void registerMaterials(IEventBus bus) {
-        HACHILITE = MaterialRegistry.register(HACHILITE_ID, def -> {
-            def.setCraftable(true);
-            def.setCastable(true);
-            def.setFluid(new ResourceLocation("tconx", "molten_hachilite"));
-            def.addTrait(new ResourceLocation("tconx", "hachilite_slow"));
-        });
+    public static void registerMaterials() {
+        // JSON側で登録するのでJavaはID宣言のみ
     }
 }
