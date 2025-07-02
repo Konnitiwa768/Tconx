666cc277b1b9827aeb3525f9504e5e65a5e08539
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-03 08:42:38 +0900

    Update ModMain.java

--- src/main/java/com/sakalti/tconx/ModMain.java
+++ src/main/java/com/sakalti/tconx/ModMain.java
@@ -12,6 +12,6 @@ public class ModMain {
     public ModMain() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ModMaterials.registerMaterials();
-        ModFluids.registerFluids();
+        ModFluids.FLUIDS.register(modEventBus);
     }
 }
