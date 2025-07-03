8ab06f266dc2534189e7e45d55f20635a3966f19
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-03 12:51:10 +0900

    Update ModMain.java

--- src/main/java/com/sakalti/tconx/ModMain.java
+++ src/main/java/com/sakalti/tconx/ModMain.java
@@ -4,7 +4,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import com.sakalti.tconx.material.hachilite.ModMaterials;
-import com.sakalti.tconx.material.hachilite.ModFluids;
 
 @Mod("tconx")
 public class ModMain {
@@ -12,6 +11,5 @@ public class ModMain {
     public ModMain() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ModMaterials.registerMaterials();
-        ModFluids.FLUIDS.register(modEventBus);
     }
 }
