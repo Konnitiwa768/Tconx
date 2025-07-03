35079d8fd7af25aa689de7e07e860931d2e6dcff
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-03 13:21:32 +0900

    Update ModMain.java

--- src/main/java/com/sakalti/tconx/ModMain.java
+++ src/main/java/com/sakalti/tconx/ModMain.java
@@ -4,11 +4,16 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import com.sakalti.tconx.material.hachilite.ModMaterials;
+import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.Modifiers;
+import com.sakalti.tconx.modifier.GlacialBindModifier;
 
 @Mod("tconx")
 public class ModMain {
-
+    public static ModifierEntry GLACIAL_BIND;
+    
     public ModMain() {
+        GLACIAL_BIND = Modifiers.register("glacial_bind", new GlacialBindModifier());
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ModMaterials.registerMaterials();
     }
