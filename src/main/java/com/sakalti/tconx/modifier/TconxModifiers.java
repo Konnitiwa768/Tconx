8a400d30902677e056409aa822594d09ceec0b7c
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-04 16:19:25 +0900

    Update TconxModifiers.java

--- src/main/java/com/sakalti/tconx/modifier/TconxModifiers.java
+++ src/main/java/com/sakalti/tconx/modifier/TconxModifiers.java
@@ -1,19 +1,18 @@
-package com.sakalti.tconx.modifier;
+package com.sakalti.tconx;
 
 import com.sakalti.tconx.modifier.GlacialBindModifier;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
 import slimeknights.tconstruct.library.modifiers.Modifier;
-import slimeknights.tconstruct.library.modifiers.util.ModifierDeferredRegistry;
+import slimeknights.tconstruct.library.modifiers.util.ModifierDeferredRegister;
+import slimeknights.tconstruct.library.modifiers.util.ModifierRegistryObject;
 
 public class TconxModifiers {
 
-    public static final DeferredRegister<Modifier> MODIFIERS =
-            DeferredRegister.create(ModifiersRegistry.MODIFIER_REGISTRY_KEY, "tconx");
+    public static final ModifierDeferredRegister MODIFIERS =
+            ModifierDeferredRegister.create("tconx");
 
-    public static final RegistryObject<Modifier> GLACIAL_BIND =
+    public static final ModifierRegistryObject<Modifier> GLACIAL_BIND =
             MODIFIERS.register("glacial_bind", GlacialBindModifier::new);
 
     public static void register(IEventBus bus) {
