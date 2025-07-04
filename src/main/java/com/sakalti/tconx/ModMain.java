7e211306bdd2d0247e55848f34d4c556162828bc
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-04 16:12:26 +0900

    Update ModMain.java

--- src/main/java/com/sakalti/tconx/ModMain.java
+++ src/main/java/com/sakalti/tconx/ModMain.java
@@ -1,7 +1,7 @@
 package com.sakalti.tconx;
 
 import net.minecraftforge.fml.common.Mod;
-import com.sakalti.tconx.modifier.TconxModifier
+import com.sakalti.tconx.modifier.TconxModifiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import com.sakalti.tconx.material.hachilite.ModMaterials;
