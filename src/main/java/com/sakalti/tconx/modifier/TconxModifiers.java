ea7aa89dcb1dc60766a11d185e36f8d9a0d804fb
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-04 16:24:07 +0900

    Update TconxModifiers.java

--- src/main/java/com/sakalti/tconx/modifier/TconxModifiers.java
+++ src/main/java/com/sakalti/tconx/modifier/TconxModifiers.java
@@ -1,21 +1,13 @@
-package com.sakalti.tconx.modifier;
+package com.sakalti.tconx;
 
 import com.sakalti.tconx.modifier.GlacialBindModifier;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.eventbus.api.IEventBus;
 import slimeknights.tconstruct.library.modifiers.Modifier;
-import slimeknights.tconstruct.library.modifiers.util.ModifierDeferredRegister;
-import slimeknights.tconstruct.library.modifiers.util.ModifierRegistryObject;
 
 public class TconxModifiers {
 
-    public static final ModifierDeferredRegister MODIFIERS =
-            ModifierDeferredRegister.create("tconx");
-
-    public static final ModifierRegistryObject<Modifier> GLACIAL_BIND =
-            MODIFIERS.register("glacial_bind", GlacialBindModifier::new);
-
-    public static void register(IEventBus bus) {
-        MODIFIERS.register(bus);
+    public static final Modifier GLACIAL_BIND = new GlacialBindModifier();
+    
+    public static void init() {
+        // 何も登録しない、自前インスタンス保持のみ
     }
 }
