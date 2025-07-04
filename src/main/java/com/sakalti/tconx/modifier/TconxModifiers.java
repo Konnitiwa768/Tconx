c86ea03b74ee6b4f9d63eff801ff1de66f072132
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-04 16:17:04 +0900

    Update TconxModifiers.java

--- src/main/java/com/sakalti/tconx/modifier/TconxModifiers.java
+++ src/main/java/com/sakalti/tconx/modifier/TconxModifiers.java
@@ -6,7 +6,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import slimeknights.tconstruct.library.modifiers.Modifier;
-import slimeknights.tconstruct.library.modifiers.ModifiersRegistry;
+import slimeknights.tconstruct.library.modifiers.util.ModifierDeferredRegistry;
 
 public class TconxModifiers {
 
