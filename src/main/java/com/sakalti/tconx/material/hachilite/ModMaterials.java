c9273754637ec0fd2ce2e57ebb33ab0eaf0adf15
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-06 10:17:44 +0900

    Update ModMaterials.java

--- src/main/java/com/sakalti/tconx/material/hachilite/ModMaterials.java
+++ src/main/java/com/sakalti/tconx/material/hachilite/ModMaterials.java
@@ -5,7 +5,7 @@ import slimeknights.tconstruct.library.materials.definition.MaterialId;
 public class ModMaterials {
 
     public static final MaterialId HACHILITE_ID = new MaterialId("tconx:hachilite");
-
+public static final MaterialId KANILITE_ID = new MaterialId("tconx:kanilite");
     public static void registerMaterials() {
         // JSON側で登録するのでJavaはID宣言のみ
     }
