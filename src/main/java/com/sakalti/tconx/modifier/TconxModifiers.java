cef3391ed8b186861799a51d0c2f12d08e423759
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-06 16:10:54 +0900

    Update TconxModifiers.java

--- src/main/java/com/sakalti/tconx/modifier/TconxModifiers.java
+++ src/main/java/com/sakalti/tconx/modifier/TconxModifiers.java
@@ -2,13 +2,15 @@ package com.sakalti.tconx.modifier;
 
 import com.sakalti.tconx.modifier.GlacialBindModifier;
 import com.sakalti.tconx.modifier.SuperMagnetModifier;
+import com.sakalti.tconx.modifier.IceBindModifier;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 
 public class TconxModifiers {
 
     public static final Modifier GLACIAL_BIND = new GlacialBindModifier();
     public static final Modifier SUPERMAGNET = new SuperMagnetModifier();
-
+    public static final Modifier GLACIAL_BIND = new GlacialBindModifier();
+    public static final Modifier ICE_BIND = new IceBindModifier();
     
     
     public static void init() {
