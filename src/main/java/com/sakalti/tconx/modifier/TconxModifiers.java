9dcba4b31dd0f4173f6f7e9f29c1aa7bfff2c20e
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-07 20:18:25 +0900

    Update TconxModifiers.java

--- src/main/java/com/sakalti/tconx/modifier/TconxModifiers.java
+++ src/main/java/com/sakalti/tconx/modifier/TconxModifiers.java
@@ -1,6 +1,7 @@
 package com.sakalti.tconx.modifier;
 
 import com.sakalti.tconx.modifier.GlacialBindModifier;
+import com.sakalti.tconx.modifier.LongsparkModifier;
 import com.sakalti.tconx.modifier.SuperMagnetModifier;
 import com.sakalti.tconx.modifier.IceBindModifier;
 import com.sakalti.tconx.modifier.LifestealModifier;
@@ -12,6 +13,7 @@ public class TconxModifiers {
     public static final Modifier SUPERMAGNET = new SuperMagnetModifier();
     public static final Modifier ICE_BIND = new IceBindModifier();
     public static final Modifier LIFESTEAL = new LifestealModifier();
+    public static final Modifier LONG_SPARK = new LongsparkModifier();
     
     public static void init() {
         // 何も登録しない、自前インスタンス保持のみ
