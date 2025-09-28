06ad3082b9cf3c24be645c159025a1a695165e1c
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-09-28 13:50:57 +0900

    Update ModNuzwats.java

--- src/main/java/com/sakalti/tconx/registry/ModNuzwats.java
+++ src/main/java/com/sakalti/tconx/registry/ModNuzwats.java
@@ -1,7 +1,6 @@
 package com.sakalti.tconx.registry;
 
 import com.sakalti.tconx.items.NuzwatItem;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Tiers;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,17 +13,17 @@ public class ModNuzwats {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "tconx");
 
     public static final RegistryObject<Item> WOODEN_NUZWAT = ITEMS.register("wooden_nuzwat",
-            () -> new NuzwatItem(Tiers.WOOD, 1.9f, 3.5f, new Item.Properties().tab(CreativeModeTabs.COMBAT)));
+            () -> new NuzwatItem(Tiers.WOOD, 1.9f, 3.5f, new Item.Properties()));
     public static final RegistryObject<Item> STONE_NUZWAT = ITEMS.register("stone_nuzwat",
-            () -> new NuzwatItem(Tiers.STONE, 2.0f, 4.2f, new Item.Properties().tab(CreativeModeTabs.COMBAT)));
+            () -> new NuzwatItem(Tiers.STONE, 2.0f, 4.2f, new Item.Properties()));
     public static final RegistryObject<Item> IRON_NUZWAT = ITEMS.register("iron_nuzwat",
-            () -> new NuzwatItem(Tiers.IRON, 2.1f, 4.9f, new Item.Properties().tab(CreativeModeTabs.COMBAT)));
+            () -> new NuzwatItem(Tiers.IRON, 2.1f, 4.9f, new Item.Properties()));
     public static final RegistryObject<Item> GOLDEN_NUZWAT = ITEMS.register("golden_nuzwat",
-            () -> new NuzwatItem(Tiers.GOLD, 2.7f, 3.5f, new Item.Properties().tab(CreativeModeTabs.COMBAT)));
+            () -> new NuzwatItem(Tiers.GOLD, 2.7f, 3.5f, new Item.Properties()));
     public static final RegistryObject<Item> DIAMOND_NUZWAT = ITEMS.register("diamond_nuzwat",
-            () -> new NuzwatItem(Tiers.DIAMOND, 2.2f, 5.6f, new Item.Properties().tab(CreativeModeTabs.COMBAT)));
+            () -> new NuzwatItem(Tiers.DIAMOND, 2.2f, 5.6f, new Item.Properties()));
     public static final RegistryObject<Item> NETHERITE_NUZWAT = ITEMS.register("netherite_nuzwat",
-            () -> new NuzwatItem(Tiers.NETHERITE, 2.3f, 6.3f, new Item.Properties().tab(CreativeModeTabs.COMBAT)));
+            () -> new NuzwatItem(Tiers.NETHERITE, 2.3f, 6.3f, new Item.Properties()));
 
     public static void register(IEventBus bus) {
         ITEMS.register(bus);
