678088b3366b25817a995fca67ed3d9f6c08d3cc
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-10-10 17:53:39 +0900

    Update and rename NuzwatDamageEnchantment.java to ModEnchantments.java

---
+++ src/main/java/com/sakalti/tconx/enchant/ModEnchantments.java
@@ -0,0 +1,44 @@
+package com.sakalti.tconx.enchant;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.entity.EquipmentSlot;
+import com.sakalti.tconx.items.NuzwatItem;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+// エンチャント登録
+public class ModEnchantments {
+    public static final DeferredRegister<Enchantment> ENCHANTMENTS =
+        DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, "tconx");
+
+    public static final RegistryObject<Enchantment> NUZWAT_DAMAGE =
+        ENCHANTMENTS.register("nuzwat_damage", NuzwatDamageEnchantment::new);
+
+    public static final RegistryObject<Enchantment> NUZWAT_UNDEAD_BANE =
+        ENCHANTMENTS.register("nuzwat_undead_bane", NuzwatUndeadBaneEnchantment::new);
+
+    // ヌズワトダメ増加
+    public static class NuzwatDamageEnchantment extends Enchantment {
+        public NuzwatDamageEnchantment() {
+            super(Rarity.UNCOMMON, EnchantmentCategory.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
+        }
+        @Override
+        public boolean canApply(ItemStack stack) {
+            return stack.getItem() instanceof NuzwatItem;
+        }
+    }
+
+    // ヌズワトアンデット特効
+    public static class NuzwatUndeadBaneEnchantment extends Enchantment {
+        public NuzwatUndeadBaneEnchantment() {
+            super(Rarity.UNCOMMON, EnchantmentCategory.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
+        }
+        @Override
+        public boolean canApply(ItemStack stack) {
+            return stack.getItem() instanceof NuzwatItem;
+        }
+    }
+}
