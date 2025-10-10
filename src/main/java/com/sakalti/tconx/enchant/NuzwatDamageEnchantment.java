ce0e59c63d05969d72c795349fdd0b15b4bbf913
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-10-10 17:28:28 +0900

    Update NuzwatDamageEnchantment.java

--- src/main/java/com/sakalti/tconx/enchant/NuzwatDamageEnchantment.java
+++ src/main/java/com/sakalti/tconx/enchant/NuzwatDamageEnchantment.java
@@ -4,6 +4,7 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraftforge.registries.ObjectHolder;
+import net.minecraft.world.item.ItemStack;
 
 public class ModEnchantments {
 
