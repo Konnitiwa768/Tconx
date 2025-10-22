ad65ad0790a0936eb7c171038552e6362c426f92
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-10-22 16:28:30 +0900

    Update ModMirzo.java

--- src/main/java/com/sakalti/tconx/registry/ModMirzo.java
+++ src/main/java/com/sakalti/tconx/registry/ModMirzo.java
@@ -17,8 +17,8 @@ public class ModMirzo {
             ITEMS.register("stone_mirzo", () -> new MirzoItem(4.4f, 1.63f, 300));
     public static final RegistryObject<Item> IRON_MIRZO =
             ITEMS.register("iron_mirzo", () -> new MirzoItem(5.4f, 1.11f, 240));
-    public static final RegistryObject<Item> GOLD_MIRZO =
-            ITEMS.register("gold_mirzo", () -> new MirzoItem(6.4f, 1.02f, 200));
+    public static final RegistryObject<Item> GOLDEN_MIRZO =
+            ITEMS.register("golden_mirzo", () -> new MirzoItem(6.4f, 1.02f, 200));
     public static final RegistryObject<Item> DIAMOND_MIRZO =
             ITEMS.register("diamond_mirzo", () -> new MirzoItem(8.0f, 0.95f, 160));
     public static final RegistryObject<Item> NETHERITE_MIRZO =
