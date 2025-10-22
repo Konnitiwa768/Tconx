6ec00fecb4b62a418ac9b5eb30be0c436ca81908
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-10-22 18:12:02 +0900

    Update MirzoItem.java

--- src/main/java/com/sakalti/tconx/items/MirzoItem.java
+++ src/main/java/com/sakalti/tconx/items/MirzoItem.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.network.chat.Component;
