7d8c27b990ed2433d6c3fb620940f7317e328424
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-10-22 17:35:09 +0900

    Update ModCreativeTabs.java

--- src/main/java/com/sakalti/tconx/registry/ModCreativeTabs.java
+++ src/main/java/com/sakalti/tconx/registry/ModCreativeTabs.java
@@ -5,33 +5,28 @@ import com.sakalti.tconx.registry.ModNuzwats;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.core.NonNullList;
 
 public class ModCreativeTabs {
 
     public static final CreativeModeTab ORIGINAL_WEAPONS = CreativeModeTab.builder()
             .title(Component.literal("Original Weapons"))
             .icon(() -> new ItemStack(ModMirzo.DIAMOND_MIRZO.get()))
-            .displayItems((displayParams, items) -> {
-                NonNullList<ItemStack> list = NonNullList.create();
-
+            .displayItems((displayParams, output) -> {
                 // Mirzo 6種類
-                list.add(new ItemStack(ModMirzo.WOODEN_MIRZO.get()));
-                list.add(new ItemStack(ModMirzo.STONE_MIRZO.get()));
-                list.add(new ItemStack(ModMirzo.IRON_MIRZO.get()));
-                list.add(new ItemStack(ModMirzo.GOLDEN_MIRZO.get()));
-                list.add(new ItemStack(ModMirzo.DIAMOND_MIRZO.get()));
-                list.add(new ItemStack(ModMirzo.NETHERITE_MIRZO.get()));
+                output.accept(ModMirzo.WOODEN_MIRZO.get());
+                output.accept(ModMirzo.STONE_MIRZO.get());
+                output.accept(ModMirzo.IRON_MIRZO.get());
+                output.accept(ModMirzo.GOLDEN_MIRZO.get());
+                output.accept(ModMirzo.DIAMOND_MIRZO.get());
+                output.accept(ModMirzo.NETHERITE_MIRZO.get());
 
                 // Nuzwat 6種類
-                list.add(new ItemStack(ModNuzwats.WOODEN_NUZWAT.get()));
-                list.add(new ItemStack(ModNuzwats.STONE_NUZWAT.get()));
-                list.add(new ItemStack(ModNuzwats.IRON_NUZWAT.get()));
-                list.add(new ItemStack(ModNuzwats.GOLDEN_NUZWAT.get()));
-                list.add(new ItemStack(ModNuzwats.DIAMOND_NUZWAT.get()));
-                list.add(new ItemStack(ModNuzwats.NETHERITE_NUZWAT.get()));
-
-                items.addAll(list); // ← こうしてまとめて追加
+                output.accept(ModNuzwats.WOODEN_NUZWAT.get());
+                output.accept(ModNuzwats.STONE_NUZWAT.get());
+                output.accept(ModNuzwats.IRON_NUZWAT.get());
+                output.accept(ModNuzwats.GOLDEN_NUZWAT.get());
+                output.accept(ModNuzwats.DIAMOND_NUZWAT.get());
+                output.accept(ModNuzwats.NETHERITE_NUZWAT.get());
             })
             .build();
 }
