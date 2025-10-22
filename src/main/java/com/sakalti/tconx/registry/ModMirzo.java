6267fee283381b15c6b6ae8c677e2df15b72b1d1
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-10-22 16:09:03 +0900

    Update ModMirzo.java

--- src/main/java/com/sakalti/tconx/registry/ModMirzo.java
+++ src/main/java/com/sakalti/tconx/registry/ModMirzo.java
@@ -11,16 +11,16 @@ public class ModMirzo {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Tconx.MOD_ID);
 
-    public static final RegistryObject<Item> MIRZO_WOOD =
-            ITEMS.register("mirzo_wood", () -> new MirzoItem(2.9f, 2.0f, 360));
-    public static final RegistryObject<Item> MIRZO_STONE =
-            ITEMS.register("mirzo_stone", () -> new MirzoItem(4.4f, 1.63f, 300));
-    public static final RegistryObject<Item> MIRZO_IRON =
-            ITEMS.register("mirzo_iron", () -> new MirzoItem(5.4f, 1.11f, 240));
-    public static final RegistryObject<Item> MIRZO_GOLD =
-            ITEMS.register("mirzo_gold", () -> new MirzoItem(6.4f, 1.02f, 200));
-    public static final RegistryObject<Item> MIRZO_DIAMOND =
-            ITEMS.register("mirzo_diamond", () -> new MirzoItem(8.0f, 0.95f, 160));
-    public static final RegistryObject<Item> MIRZO_NETHERITE =
-            ITEMS.register("mirzo_netherite", () -> new MirzoItem(11.0f, 0.88f, 120));
+    public static final RegistryObject<Item> WOOD_MIRZO =
+            ITEMS.register("wood_mirzo", () -> new MirzoItem(2.9f, 2.0f, 360));
+    public static final RegistryObject<Item> STONE_MIRZO =
+            ITEMS.register("stone_mirzo", () -> new MirzoItem(4.4f, 1.63f, 300));
+    public static final RegistryObject<Item> IRON_MIRZO =
+            ITEMS.register("iron_mirzo", () -> new MirzoItem(5.4f, 1.11f, 240));
+    public static final RegistryObject<Item> GOLD_MIRZO =
+            ITEMS.register("gold_mirzo", () -> new MirzoItem(6.4f, 1.02f, 200));
+    public static final RegistryObject<Item> DIAMOND_MIRZO =
+            ITEMS.register("diamond_mirzo", () -> new MirzoItem(8.0f, 0.95f, 160));
+    public static final RegistryObject<Item> NETHERITE_MIRZO =
+            ITEMS.register("netherite_mirzo", () -> new MirzoItem(11.0f, 0.88f, 120));
 }
