e14c2ba08e4840b7a7d18d0e6666fd02a4022aa4
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-10-25 14:00:57 +0900

    Create PierceThrustEnchantment.java

---
+++ src/main/java/com/sakalti/tconx/enchant/PierceThrustEnchantment.java
@@ -0,0 +1,16 @@
+package com.sakalti.tconx.enchantment;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+
+public class PierceThrustEnchantment extends Enchantment {
+
+    public PierceThrustEnchantment(Rarity rarity) {
+        super(rarity, EnchantmentCategory.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
+    }
+
+    public int getMaxLevel() {
+        return 2;
+    }
+}
