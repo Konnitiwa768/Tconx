6346656d943a9df488a6122338cd7b4ed110ede1
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-10-25 14:01:26 +0900

    Create HemorrhageEnchantment.java

---
+++ src/main/java/com/sakalti/tconx/enchant/HemorrhageEnchantment.java
@@ -0,0 +1,16 @@
+package com.sakalti.tconx.enchantment;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+
+public class HemorrhageEnchantment extends Enchantment {
+
+    public HemorrhageEnchantment(Rarity rarity) {
+        super(rarity, EnchantmentCategory.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
+    }
+
+    public int getMaxLevel() {
+        return 3;
+    }
+}
